c0113f163bab5391a00e46a4c15cb7800bb009a2
Changkun <sychaichangkun@gmail.com>
2017-07-19 01:00:53 +0800

    calculate wrist center

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -32,23 +32,70 @@ def handle_calculate_IK(req):
             joint_trajectory_point = JointTrajectoryPoint()
 
             # Define DH param symbols
-
+            d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+            a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+            alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
             
             # Joint angle symbols
-
+            q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
 
       
             # Modified DH params
 
-
-            
             # Define Modified DH Transformation matrix
-
+            s = { alpha0 :     0, a0 :      0, d1 : 0.75,
+                  alpha1 : -pi/2, a1 :   0.35, d2 : 0,     q2 : q2-pi/2,
+                  alpha2 :     0, a2 :   1.25, d3 : 0,
+                  alpha3 : -pi/2, a3 :  -0.54, d4 : 1.5,
+                  alpha4 :  pi/2, a4 :      0, d5 : 0,
+                  alpha5 : -pi/2, a5 :      0, d6 : 0,
+                  alpha6 : -pi/2, a6 :      0, d7 : 0.303, q7 : 0   }
 
 
             # Create individual transformation matrices
+            T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
+                           [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                           [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                           [                   0,                   0,            0,               1]])
+            T0_1 = T0_1.subs(s)
 
+
+            T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
+                           [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                           [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                           [                   0,                   0,            0,               1]])
+            T1_2 = T1_2.subs(s)
+
+            T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
+                           [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+                           [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+                           [                   0,                   0,            0,               1]])
+            T2_3 = T2_3.subs(s)
+
+            T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
+                           [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                           [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                           [                   0,                   0,            0,               1]])
+            T3_4 = T3_4.subs(s)
+
+            T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
+                           [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                           [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                           [                   0,                   0,            0,               1]])
+            T4_5 = T4_5.subs(s)
+
+            T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
+                           [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                           [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                           [                   0,                   0,            0,               1]])
+            T5_6 = T5_6.subs(s)
+
+            T6_G = Matrix([[             cos(q7),            -sin(q4),            0,              a6],
+                           [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+                           [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+                           [                   0,                   0,            0,               1]])
+            T6_G = T6_G.subs(s)
 
             
             # Extract end-effector position and orientation from request
@@ -64,8 +111,16 @@ def handle_calculate_IK(req):
      
             # Calculate joint angles using Geometric IK method
 
-		
-
+            #Homogeneous transform from base_link to end-effector
+            T0_6 = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6)
+            #Rotation matrix from base_link to gripper_link
+            R0_6 = T0_6[0:3,0:3]
+            #Gripper position
+            P = Matrix([[px],[py],[pz]])
+            #Wrist center position
+            WC = simplify(P - simplify(R0_6*Matrix([[0],[0],[d7]])))
+            print(WC)
+#### TODO
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
